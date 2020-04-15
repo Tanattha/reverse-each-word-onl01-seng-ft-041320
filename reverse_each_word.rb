42574f7313fc8303f1efc6b9da870df7751e9345
Tanattha Thuenim <tanattha.thuenim@gmail.com>
2020-04-15 22:08:50 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,4 +1,8 @@
-def reverse_each_word(words)
+def reverse_each_word(sentence)
   words.to_s.reverse
+  sentence.split
 end
 
+def reverse_each_word(sentence)
+  sentence.split.collect {|word| word.reverse}.join(" ")
+end
