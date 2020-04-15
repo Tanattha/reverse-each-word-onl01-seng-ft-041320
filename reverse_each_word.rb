f985f89fa4af69f225940a4480a5839af20dfd80
Tanattha Thuenim <tanattha.thuenim@gmail.com>
2020-04-15 22:16:41 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,7 +1,7 @@
 def reverse_each_word(sentence)
    new_array = []
   sentence.split.each do |word|
-   new_array << (word.reverse).join(" ")
+   new_array << word.reverse.join(" ")
   end
   new_array
 end
